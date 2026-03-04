57eef21f0094cb2ff2df8679d59f7cd4039b1c6c
LevTHeDever <250103977+LevTHeDever@users.noreply.github.com>
2026-03-04 21:16:22 +0100

    Added The files
    Optionaly itsjust my youtube description so bassically its useless its just to make my description colorful

---
+++ javascript.js
@@ -0,0 +1,39 @@
+const fourspan = document.getElementById("fourth");
+
+fourspan.style.color = "red";
+
+const threespan = document.getElementById("third");
+
+threespan.style.color = "#E2E8DD";
+
+const twospan = document.getElementById("second");
+
+twospan.style.color = "#697268";
+
+const onespan = document.getElementById("first");
+
+onespan.style.color = "#B7D1DA";
+
+const fivespan = document.getElementById("fifth");
+
+fivespan.style.color = "#B3C2F2";
+
+const sixspan = document.getElementById("sixth");
+
+sixspan.style.color = "#735CDD";
+
+const seventhspan = document.getElementById("seventh");
+
+seventhspan.style.color = "#2F4550";
+
+const eighthspan = document.getElementById("eighth");
+
+eighthspan.style.color = "#30343F";
+
+const ninthspan = document.getElementById("ninth");
+
+ninthspan.style.color = "#CC76A1";
+
+const tenthspan = document.getElementById("last");
+
+tenthspan.style.color = "#7B435B";
